df5281a4e04e50f90c23a54d1ed459d9af35a919
deepansh <mauryadeepansh40@gmail.com>
2024-05-11 11:02:05 +0530

    message

--- frontend/give4good/src/App.jsx
+++ frontend/give4good/src/App.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AppContext";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div>
       <AuthContextProvider>
         <Navbar></Navbar>
+        <Home></Home>
         <div>
           <Outlet />
         </div>
